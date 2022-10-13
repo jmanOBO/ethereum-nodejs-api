133be58f241302a264859b39d8e10ec27071063c
jmanOBO <okonkwoify@algosonwebtechnologies.com>
2022-10-13 01:44:03 +0100

    added cors to api

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const express=require("express");
 const cookieParser=require("cookie-parser");
-
+const cors=require("cors");
 
 const dbConnect=require("./models/dbConnect");
 
 const PORT=process.env.PORT;
 const mongodb_conn_string=process.env.MONGODB_CONNECT_STRING;
 const app=express();
-
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1", require("./routes/v1/routeHandler"));
